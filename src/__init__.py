55a7abf2d844c741e7f005c48de4133309c0255b
HC <hcc7591@mavs.uta.edu>
2025-03-05 00:34:21 -0600

    Added flask_api and image_capture service files

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+CosmiCam - Continuous Sky Recording System
+------------------------------------------
+
+A Raspberry Pi-based system that continuously records the sky, day and night,
+and broadcasts images to the local network over HTTP.
+"""
+
+__version__ = '0.1.0'
